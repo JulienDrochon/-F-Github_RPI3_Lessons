a8d22bf2a35273f5bb43c0bb55e9bf518d605ea0
Julien <julien.drochon@gmail.com>
2019-01-17 09:02:18 +0100

    new commit

--- B_RPI_Nodejs/B_A_Sensors/B_A_A_HCSR04/app.js
+++ B_RPI_Nodejs/B_A_Sensors/B_A_A_HCSR04/app.js
@@ -1,10 +1,22 @@
 var five = require('johnny-five');
 var PiIO = require('pi-io');
+var http = require('http');
+var express = require('express');
+var app=express();
+var distanceData;
 
+//---------------------------------------------- Serveur Socket.io
+app.use(express.static('public'));
+var server = http.createServer(app);
+server.listen(3000);
+var io = require('socket.io').listen(server);
+
+//---------------------------------------------- Johnny Five Board
 var board = new five.Board({
   io: new PiIO()
 });
 
+//---------------------------------------------- Set GPIO
 board.on('ready', function() {
   var proximity = new five.Proximity({
     controller: PiIO.HCSR04, // Custom controller
@@ -12,7 +24,15 @@ board.on('ready', function() {
     echoPin: 'GPIO24'
   });
 
-  proximity.on("change", function() {
-    console.log("cm: ", this.cm);
+  //---------------------------------------------- When GPIO values change…
+  io.of('/toWebBrowser').on('connection', function (socket) {
+    proximity.on("change", function() {
+      distanceData = this.cm;
+      if(distanceData > 0){
+        console.log(distanceData);
+        socket.emit('distanceData', distanceData );
+      }
+    });
+
   });
 });
